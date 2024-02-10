177fbbce2ec8c0f72cdfc693d59ed305958332a5
David <contato.davidalmeida@outlook.com>
2024-02-09 21:27:01 -0300

    🏷️ Add or update types

--- src/@types/vite-env.d.ts
+++ src/@types/vite-env.d.ts
@@ -1,7 +1,19 @@
 /// <reference types="vite/client" />
+interface Note {
+  id: string;
+  date: Date;
+  content: string;
+}
+
 interface NoteCardProps {
   note: {
+    id: string;
     date: Date;
     content: string;
   };
+  onNoteDeleted: (id: string) => void;
+}
+
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void;
 }
